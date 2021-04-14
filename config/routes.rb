6b68d63056122982972dd02887abd8c4a84da596
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-04-14 15:45:49 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'students/index'
+
+  get 'students/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
+  get 'students' => 'students#index'
+  resources :students
 end
